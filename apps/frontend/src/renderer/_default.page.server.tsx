4a823a2c4bedb1e0969214dfe3fb8646b58f3342
Torsten Hansen <torsten.michael.hansen@gmail.com>
2023-09-08 02:32:50 +0200

    add logo icon

--- apps/frontend/src/renderer/_default.page.server.tsx
+++ apps/frontend/src/renderer/_default.page.server.tsx
@@ -7,8 +7,9 @@ export async function render() {
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
+        <link rel="icon" href="/logo-icon.svg" type="image/svg+xml"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </head>
+      </head> 
       <body>
         <div id="root">
           <span id="loadID">loading ...</span>
