af89b31211fe549e1092f4946ad3c0ed857f6301
Torsten Hansen <torsten.michael.hansen@gmail.com>
2023-09-12 01:07:47 +0200

    add sign in screen

--- apps/frontend/src/components/sign-in-screen.tsx
+++ apps/frontend/src/components/sign-in-screen.tsx
@@ -1,13 +1,82 @@
 import { auth } from '#/firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { TextField } from '@kobalte/core';
+import { createSignal } from 'solid-js';
+import { AuthError, AuthErrorCodes } from 'firebase/auth';
 
 export function SignInScreen() {
-  const signIn = async () => {
+  const [email, setEmail] = createSignal('');
+  const [emailError, setEmailError] = createSignal('');
+  const [password, setPassword] = createSignal('');
+  const [passwordError, setPasswordError] = createSignal('');
+
+  const onSubmit = async (e: SubmitEvent) => {
+    e.preventDefault();
+
     try {
-      await signInWithEmailAndPassword(auth, 'test@test.com', '123456');
+      await signInWithEmailAndPassword(auth, email(), password());
     } catch (e) {
-      console.log('failure');
+      const error = e as AuthError;
+
+      switch (error.code) {
+        case AuthErrorCodes.INVALID_EMAIL:
+          setEmailError('Dies ist keine Emailadresse');
+          break;
+        case AuthErrorCodes.INVALID_PASSWORD:
+          setPasswordError('Falsches Passwort');
+          break;
+        default:
+          setEmailError(error.code);
+          setPasswordError(error.code);
+          console.log(error.code);
+      }
     }
   };
-  return <button onClick={signIn}>signIn</button>;
+
+  return (
+    <div class='w-screen h-screen flex flex-col items-center justify-center'>
+      <form onSubmit={onSubmit}>
+        <TextField.Root
+          onChange={(v) => {
+            setEmailError('');
+            setEmail(v);
+          }}
+          value={email()}
+          validationState={emailError() ? 'invalid' : 'valid'}
+        >
+          <TextField.Label>Email</TextField.Label>
+          <TextField.Input class='bg-neutral-700' />
+          <TextField.ErrorMessage class='h-4' forceMount>
+            {emailError()}
+          </TextField.ErrorMessage>
+        </TextField.Root>
+        <TextField.Root
+          onChange={(v) => {
+            setPasswordError('');
+            setPassword(v);
+          }}
+          value={password()}
+          validationState={passwordError() ? 'invalid' : 'valid'}
+        >
+          <TextField.Label>Passwort</TextField.Label>
+          <TextField.Input class='bg-neutral-700' type='password' />
+          <TextField.ErrorMessage class='h-4' forceMount>
+            {passwordError()}
+          </TextField.ErrorMessage>
+        </TextField.Root>
+        <div>
+          <button
+            disabled={
+              Boolean(emailError()) ||
+              Boolean(passwordError()) ||
+              !password() ||
+              !email()
+            }
+          >
+            Anmelden
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
